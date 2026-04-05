d9d579bcd6e6fdad80071e20bf8e475937babe7e
gtw2435z-hash <gtw2435z@gmail.com>
2026-04-05 17:51:20 +0700

    gate animations

--- Assets/Scripts/Gate Animations.cs
+++ Assets/Scripts/Gate Animations.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
-public class GateAnimations
+public class GateTrigger : MonoBehaviour
 {
-    
+    public Animator animator;
+    public string triggerName = "Open";
+    public string targetTag = "Car"; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            animator.SetTrigger(triggerName);
+        }
+    }
 }
